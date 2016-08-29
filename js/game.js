580b049ad3c606488d524b514b7665d38e07af40
Robert Washington & Matthew Moran <pair+amorobert.mjmoran123@devbootcamp.com>
2016-08-29 10:42:01 -0500

    Release 0 complete. Able to generate or accapt initial board state

---
+++ js/game.js
@@ -0,0 +1,37 @@
+var Game = function(gameString) {
+
+  var initBoard = function() {
+
+    //Generates random board positions
+    var rand1,rand2;
+
+    while (rand1 == rand2) {
+      rand1 = Math.floor(Math.random() * 16);
+      rand2 = Math.floor(Math.random() * 16);
+    }
+
+    //Generates random seed integers to insert into board
+    var seed1  = Math.floor(Math.random() * 2 + 1);
+    var seed2  = Math.floor(Math.random() * 2 + 1);
+
+    //Generates blank board 2d array structure
+    boardArray = [[0,0,0,0], [0,0,0,0], [0,0,0,0], [0,0,0,0]];
+
+    boardArray[Math.floor(rand1 / 4)][rand1 % 4] = seed1;
+    boardArray[Math.floor(rand2 / 4)][rand2 % 4] = seed2;
+
+    //Converts 2d array into string
+    var boardString = "";
+    for(var j = 0; j < 4; j++) {
+      for(var k = 0; k < 4; k++) {
+        boardString += boardArray[j][k];
+      }
+    }
+    return boardString;
+  }
+  //Takes in initial board state or generates random board state
+  this.gameState = gameString || initBoard();
+};
+
+console.log(newGame.gameState);
+console.log(newGame.gameState);
