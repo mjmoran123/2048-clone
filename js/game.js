c2e66a5e857d98e5d480dc137fcf54c0f11053c4
Robert Washington & Matthew Moran <pair+amorobert.mjmoran123@devbootcamp.com>
2016-08-29 15:07:58 -0500

    have up move working

--- js/game.js
+++ js/game.js
@@ -1,3 +1,28 @@
+var stringify = function(array) {
+  var string = "";
+    for(var j = 0; j < 4; j++) {
+      for(var k = 0; k < 4; k++) {
+        string += array[j][k];
+      }
+    }
+    return string;
+}
+
+var arrayify = function(string) {
+  var array = [];
+  array[0] = [];
+  array[1] = [];
+  array[2] = [];
+  array[3] = [];
+
+  for(var i = 0; i < 16; i++) {
+      array[Math.floor(i / 4)][i % 4] = parseInt(string.charAt(i));
+  }
+  return array;
+}
+
+
+
 var Game = function(gameString) {
 
   var initBoard = function() {
@@ -6,13 +31,13 @@ var Game = function(gameString) {
     var rand1,rand2;
 
     while (rand1 == rand2) {
-      rand1 = Math.floor(Math.random() * 16);
-      rand2 = Math.floor(Math.random() * 16);
+      rand1 = _.random(15);
+      rand2 = _.random(15);
     }
 
     //Generates random seed integers to insert into board
-    var seed1  = Math.floor(Math.random() * 2 + 1);
-    var seed2  = Math.floor(Math.random() * 2 + 1);
+    var seed1  = _.random(1,2);
+    var seed2  = _.random(1,2);
 
     //Generates blank board 2d array structure
     boardArray = [[0,0,0,0], [0,0,0,0], [0,0,0,0], [0,0,0,0]];
@@ -21,13 +46,7 @@ var Game = function(gameString) {
     boardArray[Math.floor(rand2 / 4)][rand2 % 4] = seed2;
 
     //Converts 2d array into string
-    var boardString = "";
-    for(var j = 0; j < 4; j++) {
-      for(var k = 0; k < 4; k++) {
-        boardString += boardArray[j][k];
-      }
-    }
-    return boardString;
+     return stringify(boardArray);
   }
   //Takes in initial board state or generates random board state
   this.gameState = gameString || initBoard();
@@ -57,13 +76,58 @@ Game.prototype.smash = function(array) {
     } else {
       array = this.collapse(array);
     }
-    console.log(array);
+    // console.log(array);
   }
   return array
+};
+
+Game.prototype.up = function() {
+  var currentBoard = arrayify(this.gameState);
+  var col0 = [];
+  var col1 = [];
+  var col2 = [];
+  var col3 = [];
+  for(var i = 0; i < 4; i++) {
+    for(var j = 0; j < 4; j++) {
+      if(j == 0) {
+        col0.push(currentBoard[i][j]);
+      } else if(j == 1) {
+        col1.push(currentBoard[i][j]);
+      }else if (j == 2) {
+        col2.push(currentBoard[i][j]);
+      } else if( j == 3) {
+        col3.push(currentBoard[i][j]);
+      }
+    }
+  }
+  col0 = this.smash(col0);
+  col1 = this.smash(col1);
+  col2 = this.smash(col2);
+  col3 = this.smash(col3);
+  newBoardArray = [];
+
+  newBoardArray[0] = [col0[0], col1[0], col2[0], col3[0]];
+  newBoardArray[1] = [col0[1], col1[1], col2[1], col3[1]];
+  newBoardArray[2] = [col0[2], col1[2], col2[2], col3[2]];
+  newBoardArray[3] = [col0[3], col1[3], col2[3], col3[3]];
+
+  var randI = _.random(3);
+  var randJ = _.random(3);
+
+  while(newBoardArray[randI][randJ] != 0) {
+    randI = _.random(3);
+    randJ = _.random(3);
+  }
+
+newBoardArray[randI][randJ] = _.random(1,2);
+
+this.gameState = stringify(newBoardArray);
 };
 
 
 
 var newGame = new Game();
-console.log(newGame.gameState);
-console.log(newGame.smash([0,2,4,3]));
+console.log(newGame.toString());
+console.log(newGame.up());
+console.log(newGame.toString());
+console.log(newGame.smash([0,0,2,0]));
